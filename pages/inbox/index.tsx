a9caf39a36d2e1108d36711e95ea672d1a381acd
Carsten Koch <carsten.b.koch@gmail.com>
2024-05-01 08:19:12 +0000

    fix: update inbox item issues with debouncing

--- pages/inbox/index.tsx
+++ pages/inbox/index.tsx
@@ -1,7 +1,8 @@
 import useInbox, { Inbox } from "@/api/useInbox";
 import MainLayout from "@/components/layouts/MainLayout";
 import Input from "@/components/ui-elements/form-fields/input";
-import { useState } from "react";
+import { debounce } from "lodash";
+import { FC, useEffect, useState } from "react";
 
 const makeInboxItem = (): Inbox => ({
     id: crypto.randomUUID(),
@@ -9,22 +10,68 @@ const makeInboxItem = (): Inbox => ({
     createdAt: new Date(),
 })
 
+type ApiResponse = Promise<string | undefined>;
+type CreateInboxFn = (note: string) => ApiResponse;
+type UpdateInboxFn = (id: string, note: string) => ApiResponse;
+
+const debouncedOnChange = debounce((
+    id: string,
+    note: string,
+    createInbox: CreateInboxFn,
+    updateNote: UpdateInboxFn,
+    isNew: boolean
+) => isNew ? createInbox(note) : updateNote(id, note), 1500);
+
+type InputFieldProps = {
+    inboxItem: Inbox;
+    createInbox: CreateInboxFn;
+    updateNote: UpdateInboxFn;
+    isNew: boolean;
+}
+
+const InputField: FC<InputFieldProps> = ({
+    inboxItem: { id, note },
+    createInbox,
+    updateNote,
+    isNew
+}) => {
+    const [value, setValue] = useState(note);
+
+    useEffect(() => setValue(note), [note]);
+
+    const handleUpdate = (newValue: string) => {
+        setValue(newValue);
+        debouncedOnChange(id, newValue, createInbox, updateNote, isNew);
+    }
+
+    return <Input
+        value={value}
+        onChange={handleUpdate}
+        placeholder="What is on your mind?"
+    />
+};
+
 const InboxPage = () => {
-    const {inbox, createInbox, updateNote} = useInbox();
+    const { inbox, createInbox, updateNote } = useInbox();
     const [newItem, setNewItem] = useState(makeInboxItem())
 
     const addInboxItem = async (note: string) => {
         const data = await createInbox(note);
         if (!data) return;
         setNewItem(makeInboxItem());
+        return data;
     }
-
-    const handleUpdate = (id: string) => (note: string) => id === newItem.id ? addInboxItem(note) : updateNote(id, note);
 
     return (
         <MainLayout title="Inbox" sectionName="Inbox">
-            {[...(inbox?.filter(({id}) => id !== newItem.id) || []), newItem].map(({id, note}) => (
-                <Input key={id} value={note} onChange={handleUpdate(id)} />
+            {[...(inbox?.filter(({id}) => id !== newItem.id) || []), newItem].map((item) => (
+                <InputField
+                    key={item.id}
+                    inboxItem={item}
+                    createInbox={addInboxItem}
+                    updateNote={updateNote}
+                    isNew={item.id === newItem.id}
+                />
             ))}
         </MainLayout>
     );
