fe882b087056d50ab57f59db2e46fa5a52728708
Carsten Koch <carstenbkoch@gmail.com>
2024-07-22 06:08:33 +0200

    feat: standardizing settings page

--- components/layouts/SettingsLayout.tsx
+++ components/layouts/SettingsLayout.tsx
@@ -1,24 +1,18 @@
 import { FC, ReactNode } from "react";
+import SidebarNav from "../navigation-menu/SidebarNav";
 import MainLayout from "./MainLayout";
-import SidebarNav, { ISidebarNavItems } from "../navigation-menu/SidebarNav";
 
 type SettingsLayoutProps = {
-  title: string;
   children: ReactNode;
-  sidebarNavItems: ISidebarNavItems[];
 };
 
-const SettingsLayout: FC<SettingsLayoutProps> = ({
-  title,
-  children,
-  sidebarNavItems,
-}) => {
+const SettingsLayout: FC<SettingsLayoutProps> = ({ children }) => {
   return (
-    <MainLayout title={title} sectionName={title}>
+    <MainLayout title="Profile & Settings" sectionName="Profile & Settings">
       <div className="space-y-6 p-10 pb-16">
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
           <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
+            <SidebarNav />
           </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
         </div>
