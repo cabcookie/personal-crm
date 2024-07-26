a35e84fdf9e89b24c995f31661a816af66455b39
Carsten Koch <carstenbkoch@gmail.com>
2024-07-26 16:12:51 +0200

    feat: for a learning on a person the date can be edited now

--- components/learnings/LearningComponent.tsx
+++ components/learnings/LearningComponent.tsx
@@ -3,9 +3,10 @@ import { EditorJsonContent, getEditorContent } from "@/helpers/ui-notes-writer";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import { Check, Edit, Trash2 } from "lucide-react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import PrayerStatus, { TPrayerStatus } from "../prayer/PrayerStatus";
 import NotesWriter from "../ui-elements/notes-writer/NotesWriter";
+import DateSelector from "../ui-elements/selectors/date-selector";
 import { Button } from "../ui/button";
 
 type LearningComponentProps = {
@@ -15,6 +16,7 @@ type LearningComponentProps = {
   onDelete?: () => void;
   onChange: (serializer: () => { json: EditorJsonContent }) => void;
   onStatusChange: (val: TPrayerStatus) => void;
+  onDateChange: (newDate: Date) => Promise<string | undefined>;
 };
 
 const LearningComponent: FC<LearningComponentProps> = ({
@@ -23,51 +25,71 @@ const LearningComponent: FC<LearningComponentProps> = ({
   onMakeEditable,
   onDelete,
   onChange,
+  onDateChange,
   onStatusChange,
-}) => (
-  <div className="py-2 space-y-1">
-    <h3 className="text-base md:text-lg font-bold tracking-tight flex items-center gap-2">
-      {format(learning.learnedOn, "PPP")}
-      {onMakeEditable && (
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-muted-foreground"
-          onClick={onMakeEditable}
-        >
-          {editable ? (
-            <Check className="w-5 h-5" />
-          ) : (
-            <Edit className="w-5 h-5" />
-          )}
-        </Button>
-      )}
-      {editable && (
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-muted-foreground"
-          onClick={onDelete}
-          asChild
-        >
-          <Trash2 className="w-5 h-5" />
-        </Button>
-      )}
-    </h3>
-    <PrayerStatus
-      status={learning.prayerStatus}
-      onChange={onStatusChange}
-      editable={!!editable}
-    />
-    <div className={cn(!editable && "-mx-2")}>
-      <NotesWriter
-        readonly={!editable}
-        placeholder="Document what you've learned about the person…"
-        notes={learning.learning}
-        saveNotes={(editor) => onChange(getEditorContent(editor))}
+}) => {
+  const [savingDate, setSavingDate] = useState(false);
+
+  const handleDateChange = async (newDate: Date) => {
+    setSavingDate(true);
+    console.log({ newDate });
+    await onDateChange(newDate);
+    setSavingDate(false);
+  };
+
+  return (
+    <div className="py-2 space-y-1">
+      <h3 className="text-base md:text-lg font-bold tracking-tight flex items-center gap-2">
+        {editable && (
+          <DateSelector
+            date={learning.learnedOn}
+            setDate={handleDateChange}
+            bold
+            isLoading={savingDate}
+          />
+        )}
+        {!editable && format(learning.learnedOn, "PPP")}
+        {onMakeEditable && (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-muted-foreground"
+            onClick={onMakeEditable}
+          >
+            {editable ? (
+              <Check className="w-5 h-5" />
+            ) : (
+              <Edit className="w-5 h-5" />
+            )}
+          </Button>
+        )}
+        {editable && (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-muted-foreground"
+            onClick={onDelete}
+            asChild
+          >
+            <Trash2 className="w-5 h-5" />
+          </Button>
+        )}
+      </h3>
+      <PrayerStatus
+        status={learning.prayerStatus}
+        onChange={onStatusChange}
+        editable={!!editable}
       />
+      <div className={cn(!editable && "-mx-2")}>
+        <NotesWriter
+          readonly={!editable}
+          placeholder="Document what you've learned about the person…"
+          notes={learning.learning}
+          saveNotes={(editor) => onChange(getEditorContent(editor))}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LearningComponent;
--- docs/releases/next.md
+++ docs/releases/next.md
@@ -1,9 +1,10 @@
 # Erzeugen von Einträgen über das Hauptmenü optimieren (Version :VERSION)
 
 - Über das Hauptmenü können nun Einträge erzeugt werden, die direkt die Eingabe verwenden. Der neue Eintrag kann in einem neuen Fenster geöffnet werden, wenn während der Auswahl die Command-Taste gedrückt gehalten wird.
+- Wenn etwas über eine Person gelernt wird, kann nun auch das Datum angepasst werden.
 
 ## In Arbeit
 
-- Wenn etwas über eine Person gelernt wird, kann nun auch das Datum angepasst werden.
+- Kontaktdetails können jetzt angeklickt werden und entsprechend der Kategorie wird ein Anruf getätigt, eine E-Mail geöffnet oder eine Profilseite aufgerufen.
 
 ## Geplant
