464a35deca6ca2fc078173e56281b3ac4dafb682
Carsten Koch <carstenbkoch@gmail.com>
2024-08-07 12:04:05 +0200

    feat: filter crm projects when grouped by company for projects not on hold

--- components/crm/group-projects.tsx
+++ components/crm/group-projects.tsx
@@ -1,6 +1,8 @@
+import { Project, useProjectsContext } from "@/api/ContextProjects";
 import { CrmProject } from "@/api/useCrmProjects";
 import { invertSign } from "@/helpers/functional";
 import { make2YearsRevenueText } from "@/helpers/projects";
+import { isFuture } from "date-fns";
 import {
   compact,
   filter,
@@ -13,10 +15,12 @@ import {
   sum,
   uniq,
 } from "lodash/fp";
-import { FC } from "react";
+import { FC, useState } from "react";
 import DefaultAccordionItem from "../ui-elements/accordion/DefaultAccordionItem";
 import CrmProjectAccordionItem from "../ui-elements/crm-project-details/CrmProjectAccordionItem";
 import { Accordion } from "../ui/accordion";
+import { Label } from "../ui/label";
+import { Switch } from "../ui/switch";
 import HygieneIssueBadge from "./hygiene-issue-badge";
 import { hasHygieneIssues } from "./pipeline-hygiene";
 
@@ -42,50 +46,97 @@ const companyPipeline =
       sum
     )(crmProjects) ?? 0;
 
+const filterCompanyOnHoldProjects =
+  (
+    crmProjects: CrmProject[] | null,
+    showOnHold: boolean,
+    getProjectById: (projectId: string) => Project | undefined,
+    propertyName: ValidAccountPropertyNames
+  ) =>
+  (company: string) =>
+    flow(
+      getCrmProjectsByAccount(propertyName, company),
+      some(
+        (crm) =>
+          showOnHold ||
+          !crm.projectIds.length ||
+          flow(
+            get("projectIds"),
+            map(getProjectById),
+            some(flow(get("onHoldTill"), isFuture, (b) => !b))
+          )(crm)
+      )
+    )(crmProjects);
+
 const GroupCrmProjects: FC<GroupCrmProjectsProps> = ({
   crmProjects,
   propertyName,
-}) =>
-  flow(
-    map(get(propertyName)),
-    uniq,
-    compact,
-    sortBy(flow(companyPipeline(crmProjects, propertyName), invertSign)),
-    map((company: string) => (
-      <DefaultAccordionItem
-        value={`${propertyName}-${company}`}
-        triggerTitle={company}
-        badge={
-          flow(
-            getCrmProjectsByAccount(propertyName, company),
-            some(hasHygieneIssues)
-          )(crmProjects) && <HygieneIssueBadge />
-        }
-        triggerSubTitle={[
-          `${flow(
-            getCrmProjectsByAccount(propertyName, company),
-            size
-          )(crmProjects)} projects`,
-          flow(
-            companyPipeline(crmProjects, propertyName),
-            make2YearsRevenueText
-          )(company),
-        ]}
-      >
-        <Accordion type="single" collapsible>
-          {flow(
-            getCrmProjectsByAccount(propertyName, company),
-            map(({ id }: CrmProject) => (
-              <CrmProjectAccordionItem
-                key={id}
-                crmProjectId={id}
-                showProjects
-              />
-            ))
-          )(crmProjects)}
-        </Accordion>
-      </DefaultAccordionItem>
-    ))
-  )(crmProjects);
+}) => {
+  const [showOnHold, setShowOnHold] = useState(false);
+  const { getProjectById } = useProjectsContext();
+
+  return (
+    <>
+      <div className="mt-2 flex items-center space-x-2">
+        <Switch
+          id="show-onhold"
+          checked={showOnHold}
+          onCheckedChange={setShowOnHold}
+        />
+        <Label htmlFor="show-onhold">Include projects on hold</Label>
+      </div>
+
+      {flow(
+        map(get(propertyName)),
+        uniq,
+        compact,
+        sortBy(flow(companyPipeline(crmProjects, propertyName), invertSign)),
+        filter(
+          filterCompanyOnHoldProjects(
+            crmProjects,
+            showOnHold,
+            getProjectById,
+            propertyName
+          )
+        ),
+        map((company: string) => (
+          <DefaultAccordionItem
+            value={`${propertyName}-${company}`}
+            triggerTitle={company}
+            badge={
+              flow(
+                getCrmProjectsByAccount(propertyName, company),
+                some(hasHygieneIssues)
+              )(crmProjects) && <HygieneIssueBadge />
+            }
+            triggerSubTitle={[
+              `${flow(
+                getCrmProjectsByAccount(propertyName, company),
+                size
+              )(crmProjects)} projects`,
+              flow(
+                companyPipeline(crmProjects, propertyName),
+                make2YearsRevenueText
+              )(company),
+            ]}
+          >
+            <Accordion type="single" collapsible>
+              {flow(
+                getCrmProjectsByAccount(propertyName, company),
+                map(({ id }: CrmProject) => (
+                  <CrmProjectAccordionItem
+                    key={id}
+                    crmProjectId={id}
+                    showProjects
+                  />
+                ))
+              )(crmProjects)}
+            </Accordion>
+          </DefaultAccordionItem>
+        ))
+      )(crmProjects)}
+    </>
+  );
+};
 
 export default GroupCrmProjects;
--- docs/releases/next.md
+++ docs/releases/next.md
@@ -1,3 +1,3 @@
-# Fehlerbehebungen auf Aktivität-Deteilseite (Version :VERSION)
+# CRM Projekte in Wartestellung ausblenden (Version :VERSION)
 
-- Projekte können nun wieder hinzugefügt werden.
+- CRM Projekte die ausschließlich mit Projekten verknüpft sind, die im Moment stummgeschaltet sind (On Hold), werden ausgeblendet. Es sei denn der Filter „Include projects on hold“ ist aktiviert.
