e09e459edfc4bd5313f2a29b2c2b61dc36b7a7d4
Carsten Koch <carstenbkoch@gmail.com>
2024-08-08 14:32:27 +0200

    fix: improved project recommendations in meetings

--- components/meetings/meeting-project-recommender.tsx
+++ components/meetings/meeting-project-recommender.tsx
@@ -1,27 +1,12 @@
 import { useAccountsContext } from "@/api/ContextAccounts";
-import { Project, useProjectsContext } from "@/api/ContextProjects";
+import { useProjectsContext } from "@/api/ContextProjects";
+import useMeetingProjectRecommendation from "@/api/useMeetingProjectRecommendation";
 import { Meeting } from "@/api/useMeetings";
-import usePersonActivities from "@/api/usePersonActivities";
-import { compact, filter, flatMap, flow, get, map, uniq } from "lodash/fp";
-import { FC, useEffect, useState } from "react";
+import { compact, filter, flatMap, flow, get, map } from "lodash/fp";
+import { FC } from "react";
 
 const filterOutProjectIds = (avoidIds: string[] | undefined) => (id: string) =>
   !avoidIds?.includes(id);
-
-const MeetingPersonActivities = (personId: string) => {
-  const { activities } = usePersonActivities(personId);
-  return activities;
-};
-
-const useMeetingActivities = (participantIds: string[] | undefined) => {
-  if (!participantIds) return undefined;
-  return flow(
-    flatMap(MeetingPersonActivities),
-    compact,
-    flatMap(get("projectIds")),
-    uniq
-  )(participantIds);
-};
 
 type MeetingProjectRecommenderProps = {
   meeting?: Meeting | undefined;
@@ -32,15 +17,16 @@ const MeetingProjectRecommender: FC<MeetingProjectRecommenderProps> = ({
   meeting,
   addProjectToMeeting,
 }) => {
-  const projectIds = useMeetingActivities(meeting?.participantIds);
+  const { projectIds } = useMeetingProjectRecommendation(meeting);
   const { getProjectById } = useProjectsContext();
   const { getAccountNamesByIds } = useAccountsContext();
-  const [projects, setProjects] = useState<Project[] | undefined>(undefined);
 
-  useEffect(() => {
-    {
-      flow(
-        compact,
+  return (
+    <div className="px-1 md:px-2 text-sm text-muted-foreground">
+      <span className="font-semibold">
+        Add these projects where participants contributed:
+      </span>
+      {flow(
         filter(
           filterOutProjectIds(
             flow(get("activities"), flatMap(get("projectIds")))(meeting)
@@ -49,27 +35,17 @@ const MeetingProjectRecommender: FC<MeetingProjectRecommenderProps> = ({
         map(getProjectById),
         compact,
         filter((p) => !p.done),
-        setProjects
-      )(projectIds);
-    }
-  }, [getProjectById, meeting, projectIds]);
-
-  return (
-    <div className="px-1 md:px-2 text-sm text-muted-foreground">
-      <span className="font-semibold">
-        Add these projects where participants contributed:
-      </span>
-      {projects?.map((p) => (
-        <span key={p.id}>
+        map(({ id, project, accountIds }) => (
           <span
+            key={id}
             className="pl-1 md:pl-2 hover:text-primary hover:underline hover:underline-offset-2 hover:cursor-pointer"
-            onClick={() => addProjectToMeeting(p.id)}
+            onClick={() => addProjectToMeeting(id)}
           >
-            {p.project}
-            {p.accountIds && ` (${getAccountNamesByIds(p.accountIds)})`}
+            {project}
+            {accountIds && ` (${getAccountNamesByIds(accountIds)})`}
           </span>
-        </span>
-      ))}
+        ))
+      )(projectIds)}
     </div>
   );
 };
