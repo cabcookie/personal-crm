9b96b518342cf8fd23ed82f9f03de1059f7ba5e5
Carsten Koch <carstenbkoch@gmail.com>
2024-11-01 18:54:10 +0100

    fix: change backend handling of table protection

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,32 +1,46 @@
 import { defineBackend } from "@aws-amplify/backend";
-// import { RemovalPolicy } from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
 import { auth } from "./auth/resource";
-import { data } from "./data/resource";
+import { data, tablesWithDeleteProtection } from "./data/resource";
 import { storage } from "./storage/resource";
 
-// const backend = defineBackend({
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
 });
 
-// const backendType = backend.auth.resources.userPool.node.tryGetContext(
-//   "amplify-backend-type"
-// );
-// if (backendType !== "sandbox") {
-//   const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
+/**
+ * Ensure that when new tables are moved into production, they do not initially
+ * have Delete Protection and Backup enabled. CloudFormation always runs into
+ * problems if these options are enabled from the start, and Amplify deployments
+ * will fail.
+ *
+ * Therefore, we keep a list of `tablesWithDeleteProtection` in each schema and
+ * have to manually add the tables we want to enable delete protection for.
+ *
+ * In sandbox environments, we also need to disable delete protection for tables
+ * where we change indexes, as the tables will be dropped and re-created, as this
+ * is faster than deleting and creating indexes. With delete protection enabled
+ * for such tables, deployments will fail. This happens when we add indexes
+ * ourselves, but also when we add/change relations to other tables, as this
+ * causes indexes to be created as well.
+ */
 
-//   /**
-//    * Ensure that new tables when moved into production will at first not have
-//    * deletion protection and backup enabled. CloudFormation always runs into
-//    * issues when enabled right from the get-go and Amplify deployments fail.
-//    */
-//   // const { PersonRelationship: _pr, ...restTables } = amplifyDynamoDbTables;
+const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
+const backendType = backend.auth.resources.userPool.node.tryGetContext(
+  "amplify-backend-type"
+);
 
-//   Object.values(amplifyDynamoDbTables).forEach((table) => {
-//     table.pointInTimeRecoveryEnabled = true;
-//     table.deletionProtectionEnabled = true;
-//     table.applyRemovalPolicy(RemovalPolicy.RETAIN);
-//   });
-// }
+/**
+ * keep only the properties (table names) of amplifyDynamoDbTables for which
+ * their key name is in tablesWithDeleteProtection
+ */
+
+Object.keys(amplifyDynamoDbTables).forEach((key) => {
+  const setting = tablesWithDeleteProtection.includes(key);
+  amplifyDynamoDbTables[key].deletionProtectionEnabled =
+    backendType === "sandbox" ? false : setting;
+  amplifyDynamoDbTables[key].pointInTimeRecoveryEnabled = setting;
+  amplifyDynamoDbTables[key].applyRemovalPolicy(RemovalPolicy.RETAIN);
+});
