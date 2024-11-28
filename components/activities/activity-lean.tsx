abca5eb1d96ce022d28db55ff8ca075799068729
Carsten Koch <carstenbkoch@gmail.com>
2024-11-28 19:47:27 +0100

    refactor: component for lean activity

---
+++ components/activities/activity-lean.tsx
@@ -0,0 +1,43 @@
+import PersonNoteMeeting from "@/components/people/PersonNoteMeeting";
+import PersonNoteProject from "@/components/people/PersonNoteProject";
+import NotesEditor from "@/components/ui-elements/editors/notes-editor/NotesEditor";
+import { format } from "date-fns";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+import { FC } from "react";
+
+interface Activity {
+  id: string;
+  finishedOn: Date;
+  projectIds: string[];
+  meetingId?: string;
+}
+
+type LeanActivitiyProps = {
+  activity: Activity;
+  readonly?: boolean;
+};
+
+const LeanActivitiy: FC<LeanActivitiyProps> = ({ activity, readonly }) => {
+  return (
+    <div key={activity.id} className="space-y-2">
+      <div className="font-semibold text-base">
+        {format(activity.finishedOn, "PPp")}
+        <Link href={`/activities/${activity.id}`}>
+          <ExternalLink className="inline-block ml-2 w-5 h-5 -translate-y-0.5 text-muted-foreground hover:text-primary" />
+        </Link>
+      </div>
+
+      <div className="text-sm text-muted-foreground">
+        {activity.projectIds.map((pId) => (
+          <PersonNoteProject key={pId} projectId={pId} />
+        ))}
+        <PersonNoteMeeting meetingId={activity.meetingId} />
+      </div>
+
+      <NotesEditor activityId={activity.id} readonly={readonly} />
+    </div>
+  );
+};
+
+export default LeanActivitiy;
--- components/people/PersonNotes.tsx
+++ components/people/PersonNotes.tsx
@@ -1,15 +1,10 @@
 import { useProjectsContext } from "@/api/ContextProjects";
 import usePersonActivities, { PersonActivity } from "@/api/usePersonActivities";
-import { format } from "date-fns";
 import { flatMap, flow } from "lodash/fp";
-import { ExternalLink } from "lucide-react";
-import Link from "next/link";
 import { FC } from "react";
+import LeanActivitiy from "../activities/activity-lean";
 import DefaultAccordionItem from "../ui-elements/accordion/DefaultAccordionItem";
 import LoadingAccordionItem from "../ui-elements/accordion/LoadingAccordionItem";
-import NotesEditor from "../ui-elements/editors/notes-editor/NotesEditor";
-import PersonNoteMeeting from "./PersonNoteMeeting";
-import PersonNoteProject from "./PersonNoteProject";
 
 type PersonNotesProps = {
   personId?: string;
@@ -40,23 +35,7 @@ const PersonNotes: FC<PersonNotesProps> = ({ showNotes, personId }) => {
     >
       <div className="space-y-6">
         {activities?.map((a) => (
-          <div key={a.id} className="space-y-2">
-            <div className="font-semibold text-base">
-              {format(a.finishedOn, "PPp")}
-              <Link href={`/activities/${a.id}`}>
-                <ExternalLink className="inline-block ml-2 w-5 h-5 -translate-y-0.5 text-muted-foreground hover:text-primary" />
-              </Link>
-            </div>
-
-            <div className="text-sm text-muted-foreground">
-              {a.projectIds.map((pId) => (
-                <PersonNoteProject key={pId} projectId={pId} />
-              ))}
-              <PersonNoteMeeting meetingId={a.meetingId} />
-            </div>
-
-            <NotesEditor activityId={a.id} readonly />
-          </div>
+          <LeanActivitiy key={a.id} activity={a} readonly />
         ))}
       </div>
     </DefaultAccordionItem>
